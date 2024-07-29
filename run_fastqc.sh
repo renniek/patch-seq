44795a8a4f54e056b77fce48c760e15faba726e3
Rennie Kendrick <58533662+renniek@users.noreply.github.com>
2024-07-29 12:33:42 -0700

    Update run_fastqc.sh
    Change it to be able to run on Sherlock, and to compile all fastqc files into one interpretable html file with multiqc

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
-dir='/Volumes/sfowen/Data/scRNA_Data/E200013499-SGSC-Owen-JC-22666/'
+#!/usr/bin/bash
+#SBATCH --job-name=fastqc
+#SBATCH --output=fastqc.%j.out
+#SBATCH --error=fastqc.%j.err
+#SBATCH --time=01:00:00
+#SBATCH -p normal
+#SBATCH -c 1
+#SBATCH --mem=16GB
 
-#conda activate RNA-seq
-
-source /Users/caper/Desktop/Owen_PatchSeqAnalysis/venv.sh
+dir='/oak/stanford/groups/sfowen/Data/scRNA_Data/E200013499-SGSC-Owen-JC-22666/'
+module load biology
+module load fastqc 
 
 for entry in $dir/*".fastq.gz"; do 
 	if [ -f "${entry%%.*}_fastqc.html" ]
@@ -14,3 +20,11 @@ for entry in $dir/*".fastq.gz"; do
 		fastqc $entry -d . -o .
 	fi
 done
+
+# activate virtual environment to compile fastqc files into a report using multiqc
+source RNA_seq/bin/activate 
+
+multiqc .
+
+
+
